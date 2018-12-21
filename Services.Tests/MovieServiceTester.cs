09d90dcfc519a8993d1b7abf65ba194b337ba954
THOMAS FERNEZ <TFE15965@open-groupe.com>
2018-12-21 15:29:09 +0100

    Creation de test avec des mocks

---
+++ Services.Tests/MovieServiceTester.cs
@@ -0,0 +1,37 @@
+﻿using DataModel.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Services.Interfaces;
+using Services.Repositories;
+
+namespace Services.Tests
+{
+    [TestClass]
+    public class MovieServiceTester
+    {
+        private IMovieRepository _movieRepository;
+        private IMovieService _movieService;
+
+        public MovieServiceTester()
+        {
+            _movieRepository = new MovieRepository(null);
+            _movieService = new MovieService(_movieRepository);
+        }
+
+        [TestMethod]
+        public void GivenNoMovieWithSameTitle_WhenAddingNewMovie_ThenMovie()
+        {
+            var mock = new Mock<IMovieRepository>();
+            var movie = new Movie() { Title = "ok", Description = "description" };
+
+            mock.Setup(p => p.GetByTitle(It.IsAny<string>()))
+                .Returns<Movie>(null);
+            mock.Setup(p => p.Add(It.IsAny<Movie>()))
+                .Returns(movie);
+
+            var service = new MovieService(mock.Object);
+            var dbMovie = service.Add(movie);
+            Assert.IsTrue(dbMovie != null);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using DataModel.Entities;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>()
+            {
+                new Movie() {Title = "titre 1", Description = "description du premier film" },
+                new Movie() {Title = "titre 2", Description = "description du deuxième film" },
+            };
+
+            return View(movies);
+        }
+    }
+}
