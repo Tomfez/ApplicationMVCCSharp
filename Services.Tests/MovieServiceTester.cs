bf560a088d9bad10f594c15dd1e1718664271c49
THOMAS FERNEZ <TFE15965@open-groupe.com>
2018-12-21 16:10:45 +0100

    Test si film avec le meme titre

--- Services.Tests/MovieServiceTester.cs
+++ Services.Tests/MovieServiceTester.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Services.Interfaces;
 using Services.Repositories;
+using System;
 
 namespace Services.Tests
 {
@@ -19,7 +20,7 @@ namespace Services.Tests
         }
 
         [TestMethod]
-        public void GivenNoMovieWithSameTitle_WhenAddingNewMovie_ThenMovie()
+        public void GivenNoMovieWithSameTitle_WhenAddingNewMovie_ThenMovieFromRepository()
         {
             var mock = new Mock<IMovieRepository>();
             var movie = new Movie() { Title = "ok", Description = "description" };
@@ -33,5 +34,37 @@ namespace Services.Tests
             var dbMovie = service.Add(movie);
             Assert.IsTrue(dbMovie != null);
         }
+
+        [TestMethod]
+        public void GivenMovieWithSameTitle_WhenAddingNewMovie_ThenMovieFromRepository()
+        {
+            var mock = new Mock<IMovieRepository>();
+            var movie = new Movie() { Title = "test film", Description = "description" };
+
+            mock.Setup(p => p.GetByTitle(It.IsAny<string>()))
+            .Returns(movie);
+            mock.Setup(p => p.Add(It.IsAny<Movie>()))
+                .Returns(movie);
+
+            var service = new MovieService(mock.Object);
+            var dbMovie = service.Add(movie);
+            Assert.IsNull(dbMovie);
+        }
+
+        //[TestMethod]
+        //public void GivenMovieWithYearLessThan1980_WhenAddingNewMovie_ThenMovieFromRepository()
+        //{
+        //    var mock = new Mock<IMovieRepository>();
+        //    var movie = new Movie() { Title = "test film", Description = "description", ReleasedDate = new DateTime(1979, 02, 02) };
+
+        //    mock.Setup(p => p.CheckDate(It.IsAny<DateTime>()))
+        //    .Returns(movie.ReleasedDate.Year);
+        //    //mock.Setup(p => p.CheckDate(It.IsAny<Movie>()))
+        //    //    .Returns(movie);
+
+        //    var service = new MovieService(mock.Object);
+        //    var dbMovie = service.Add(movie);
+        //    Assert.IsFalse(dbMovie);
+        //}
     }
 }
